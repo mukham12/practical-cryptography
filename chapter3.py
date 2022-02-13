4a2f5c6cce44e5da5a789c7f3741b5f6ac7be811
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-02-13 16:26:54 -0500

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -5,3 +5,4 @@ key = os.urandom(16)
 
 # Never use ECB mode, it is not secure
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
