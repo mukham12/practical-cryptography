a957c81af118ae838f6590c9918eb6dfddf88480
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-02-14 07:45:46 -0500

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -1,8 +1,6 @@
 # Learning about symmetric encryption
 import os
 
-key = os.urandom(16)
-
 # Never use ECB mode, it is not secure
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
