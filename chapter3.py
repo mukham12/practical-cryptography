2e86c87f580ea0bed3db252bd0b078d0aea18cd8
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-02-16 11:35:49 -0500

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -3,3 +3,4 @@
 # Never use ECB mode, it is not secure
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+import os
