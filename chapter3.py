ac43fc065d8830824253890c41389c3e1f45c2b0
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-03-05 17:44:56 -0500

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -1,6 +1,5 @@
 # Learning about symmetric encryption
 
-# Never use ECB mode, it is not secure
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
