c1a6d2369c484e0caf9f74d06a58da75b0015adf
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-03-10 18:22:56 -0500

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -1,5 +1,3 @@
-# Learning about symmetric encryption
-
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
