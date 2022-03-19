d378b585ecfed68cbc75d101a810897c53a14244
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-03-19 08:24:25 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -3,9 +3,6 @@ from cryptography.hazmat.backends import default_backend
 import os
 
 key = os.urandom(16)
-aesCipher = Cipher(algorithms.AES(key),
-                   modes.ECB(),
-                   backend=default_backend())
 
 aesEncryptor = aesCipher.encryptor()
 aesDecryptor = aesCipher.decryptor()
