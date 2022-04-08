d593af043c2d7a61866bc1b662df4aae5f743dae
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-04-08 09:15:57 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -34,7 +34,6 @@ for index, kat in enumerate(nist_kats):
     got_ciphertext = ciphertext_bytes.hex()
     
     result = "[PASS]" if got ciphertext == want_ciphertext else "[FAIL]"
-    print(f'Test {index}. Expected {want_ciphertext}. Got {got_ciphertext}. Result {result}.')
 
 
 message = b''''''
