44f099811ac45359f7f11d21ff21608a5ec26ce4
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-04-18 17:40:59 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -9,8 +9,6 @@ aesDecryptor = aesCipher.decryptor()
 
 nist_kats = [('f34481ec3cc627bacd5dc3fb08f273e6',
               '0336763e966d92595a567cc9ce537f5e'),
-             ('9798c4640bad75c7c3227db910174e72',
-              'a9a1631bf4996954ebc093957b234589'),
             ]
 
 # Alice and Bob's shared secret key
