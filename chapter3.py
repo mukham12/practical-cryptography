9a210af549f8cc361221ccfd6ce556c124371c91
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-03 20:21:00 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -3,12 +3,12 @@ from cryptography.hazmat.backends import default_backend
 import os
 
 key = os.urandom(32)
-iv = os.urandom(16)
+
 
 test_key = bytes.fromhex('00112233445566778899AABBCCDDEEFF')
 
 aesCipher = Cipher(algorithms.AES(test_key),
-                   modes.CBC(iv),
+                   modes.EBC(),
                    backend=default_backend())
 
 
