78d72e4241c305d703d5abe4730078030f956191
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-05 12:52:04 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -4,11 +4,8 @@ import os
 
 key = os.urandom(32)
 
-
 test_key = bytes.fromhex('00112233445566778899AABBCCDDEEFF')
 
 aesCipher = Cipher(algorithms.AES(test_key),
                    modes.EBC(),
                    backend=default_backend())
-
-
