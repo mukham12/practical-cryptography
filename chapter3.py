469f047d66c6ca383818bc30cdb3742528a1c9eb
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-06 20:09:45 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -2,8 +2,6 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-key = os.urandom(32)
-
 test_key = bytes.fromhex('00112233445566778899AABBCCDDEEFF')
 
 aesCipher = Cipher(algorithms.AES(test_key),
