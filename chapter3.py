26a2a84675193b05118735f28af230335c942a60
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-07 06:41:27 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -1,6 +1,5 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-import os
 
 test_key = bytes.fromhex('00112233445566778899AABBCCDDEEFF')
 
