dda4da07778feec8b972b0ebef468b3b43d1a4bc
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-13 20:03:00 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -2,8 +2,6 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-test_key = bytes.fromhex('00112233445566778899AABBCCDDEEFF')
-
 aesCipher = Cipher(algorithms.AES(test_key),
                    modes.EBC(),
                    backend=default_backend())
