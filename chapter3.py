45193638e9ef0d4b262915f7c8b4f6d5948e7aa8
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-16 19:13:04 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -2,13 +2,13 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-aesCipher = Cipher(algorithms.AES(test_key),
-                   modes.EBC(),
-                   backend=default_backend())
-
+aesCipher = 
 class EncryptionManager:
     self.key = os.urandom(32)
     self.iv = os.urandom(16)
 
     def encrypt_message(self, message):
-        pass
+        encryptor = Cipher(algorithms.AES(test_key),
+                           modes.EBC(),
+                           backend=default_backend())
+
