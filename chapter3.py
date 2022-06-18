782a2b39945d92d60b3ab4c6cb6087164d8a746b
Muhammad K <55890311+mukham12@users.noreply.github.com>
2022-06-17 20:07:03 -0400

    Update chapter3.py

--- chapter3.py
+++ chapter3.py
@@ -2,7 +2,6 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 import os
 
-aesCipher = 
 class EncryptionManager:
     self.key = os.urandom(32)
     self.iv = os.urandom(16)
